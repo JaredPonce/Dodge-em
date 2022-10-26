f2b6c7d3d9f6f717ebf3b88a786dae0cef3a9924
Another Commit <104458411+anothercommit@users.noreply.github.com>
2022-10-26 11:40:22 -0300

    anashe

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let player, car;
-let callSpawner;
+let callSpawner, refreshCaller;
 let difficulty = 1000,
     carSpeed = 4,
     maxCars = 1;
@@ -11,17 +11,18 @@ let playerState = ALIVE;
 
 window.addEventListener("devicemotion", function (e) {
     x = parseInt(e.accelerationIncludingGravity.x) * -2.5;
+    y = parseInt(e.accelerationIncludingGravity.x) * 1;
 });
 
 function setup() {
     createCanvas(400, 800);
 
-    player = new Sprite(300, 700, 40, 70, "dynamic");
+    player = new Sprite(300, 600, 40, 70, "dynamic");
     player.color = "blue";
 
     car = new Group();
 
-    callSpawner = setInterval(spawnCar, difficulty, 1, carSpeed);
+    callSpawner = setInterval(spawnCar, difficulty, carSpeed);
     refreshCaller = setInterval(increaseDifficulty, 1000);
 }
 
@@ -31,6 +32,7 @@ function draw() {
 
         console.log(carSpeed);
         player.vel.x = x;
+        player.vel.y = y;
 
         if (player.collides(car)) gameOverScreen();
 
@@ -39,22 +41,29 @@ function draw() {
             : player.position.x < 0
             ? (player.position.x = 0)
             : (player.position.x = player.position.x);
-    } else if (playerState == DEAD) {
+
+        player.position.y <= 500
+            ? (player.position.y = 500)
+            : player.position.y < 800
+            ? (player.position.y = 800)
+            : (player.position.y = player.position.y);
     }
+
+    // else if (playerState == DEAD) {
+    // }
 }
 
-function spawnCar(amount, vel) {
-    new car.Sprite(random(25, 375), -50, 40, 70);
+function spawnCar(vel) {
+    new car.Sprite(random(25, 375), -50, 40, 70).vel = vel;
     car.color = "red";
-    car.vel.y = vel;
 }
 
 function increaseDifficulty() {
     clearInterval(callSpawner);
-    if (difficulty >= 600) difficulty += -50;
-    if (carSpeed <= 8) carSpeed += 0.05;
+    if (difficulty >= 500) difficulty += -50;
+    if (carSpeed <= 10) carSpeed += 0.05;
 
-    callSpawner = setInterval(spawnCar, difficulty, 1, carSpeed);
+    callSpawner = setInterval(spawnCar, difficulty, carSpeed);
 }
 
 function gameOverScreen() {
