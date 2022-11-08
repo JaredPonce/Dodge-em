bbc56d2d25790a98da4d3af418e7ef90635d9ece
EmiMolina248 <eemolinaperyro@escuelasproa.edu.ar>
2022-11-08 11:17:07 -0300

    a

--- scripts/main.js
+++ scripts/main.js
@@ -18,8 +18,8 @@ function setup() {
     player = new Sprite(width / 2, height / 2, 50, 50, "dynamic");
     player.color = "white";
 
-    leftBorder = new Sprite(0, 0, 50, windowHeight, "static");
-    rightBorder = new Sprite(windowWidth, 0, 50, windowHeight, "static");
+    leftBorder = new Sprite(0, height / 2, 50, height, "static");
+    rightBorder = new Sprite(width, height / 2, 50, height, "static");
 }
 
 function draw() {
