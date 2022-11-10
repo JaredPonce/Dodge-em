6192c5870bd4505ba5f9f905408cca7fc0ad76ac
anothercommit <lack_of_names@proton.me>
2022-11-10 20:52:21 -0300

    😎

--- scripts/main.js
+++ scripts/main.js
@@ -26,6 +26,8 @@ function setup() {
     // shootButton.size(200, 200);
     // shootButton.mousePressed(shoot);
     // shootButton.color = color('rgba(255, 255, 255, 50)');
+    
+    if (player.positon.y >= height / 2) player.positon.y == height / 2;
 }
 
 function draw() {
